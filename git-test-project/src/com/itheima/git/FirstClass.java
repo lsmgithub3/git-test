1bae6985944573881e978786d3c0a7f13579a441
zhangsan <zhangsan@itcast.cn>
2021-05-28 15:06:29 +0800

    修改本地java工程

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -3,5 +3,7 @@ package com.itheima.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
+        System.out.println("丝丝心动");
+        System.out.println("我的莫格利男孩");
     }
 }
